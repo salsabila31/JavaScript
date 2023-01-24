7f90e71f503553f96239bf52593dc5e1d0358788
salsabila31 <110610642+salsabila31@users.noreply.github.com>
2023-01-24 10:15:37 +0700

    Add files via upload

---
+++ JavaScript-39/ajax-jquery/php/insert.php
@@ -0,0 +1,23 @@
+<?php 
+
+require_once "koneksi.php";
+
+$data = stripslashes(file_get_contents("php://input"));
+$dataPelanggan = json_decode($data,true);
+
+$pelanggan = $dataPelanggan['pelanggan'];
+$alamat = $dataPelanggan['alamat'];
+$telp = $dataPelanggan['telp'];
+
+if (!empty($pelanggan) and !empty($alamat) and !empty($telp)) {
+    $sql = "INSERT INTO tblpelanggan VALUES('','$pelanggan','$alamat','$telp')";
+    if ($result = mysqli_query($con,$sql)) {
+        echo "Data Sudah Disimpan!";
+    } else {
+        echo "Data Gagal Disimpan !";
+    }
+}else {
+    echo "Data Kosong";
+}
+
+?>
